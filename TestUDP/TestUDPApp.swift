06f2f61bd9701b33288c9056508c894712fa4bf5
Jedrzej <42713788+Extollite@users.noreply.github.com>
2020-10-15 15:43:26 +0200

    Initial Commit

---
+++ TestUDP/TestUDPApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestUDPApp.swift
+//  TestUDP
+//
+//  Created by Extollite on 15/10/2020.
+//
+
+import SwiftUI
+
+@main
+struct TestUDPApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
